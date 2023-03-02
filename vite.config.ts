f8df6d6472d55fc0b2686036bb6a7d4d9047c492
xhtbtk <xhrbtk@gmail.com>
2023-03-02 11:02:55 +0800

    feat:使用tsx 代替template

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/publish/dist/',
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps: true
+    })
+  ],
+  
 })
