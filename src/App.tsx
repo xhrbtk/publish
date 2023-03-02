1d901621d805f4291183470a87e0f2a72b5053b4
xhtbtk <xhrbtk@gmail.com>
2023-03-02 20:42:42 +0800

    feat:css初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import { defineComponent} from "vue";
 import { RouterView } from "vue-router"
+import './App.scss'
 export const App = defineComponent({
     setup() {
         
         return () => (
-        <>
-           <header>导航
-            <ul>
-                <li>
-                    <router-link to="/">foo</router-link>  
-                </li>
-                <li>
-                    <router-link to="/about"> about</router-link>
-                </li>
-            </ul>
-           </header>
            <div>
             <RouterView />
            </div>
-           <footer>页脚</footer>
-        </>
         )
     }
 })
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,15 +1,22 @@
 import { defineComponent, ref } from "vue";
 import { RouterView } from "vue-router";
+import s from './Welcome.module.scss'
+import logo from '../assets/icons/mangosteen.svg'
+
 
 export const Welcome = defineComponent({
     setup() {
         return () => (
-        <>
-           <div >
-            welcome
-            <RouterView></RouterView>
-           </div>
-        </>
+        <div class={s.wrapper}>
+           <header>
+                <img src={logo} alt="" />
+                <h1>山竹记账</h1>
+           </header>
+           <main >
+                <RouterView></RouterView>
+           </main>
+           <footer>buttons</footer>
+        </div>
         )
     }
 })
