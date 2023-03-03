ac2755efb06add54b4fc8e147f97af3669371504
xhtbtk <xhrbtk@gmail.com>
2023-03-03 15:59:04 +0800

    fix:修改默认路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -9,11 +9,11 @@ import { Welcome } from '../views/Welcome'
 
 
 export const routes: RouteRecordRaw[] = [
-    { path: '/', component: Foo },
-    { path: '/about', component: Bar },
+    { path: '/', redirect:  '/welcome'},
     { path: '/welcome', 
       component: Welcome,
       children: [
+        { path: '', redirect: '/welcome/1' },
         { path: '1', component: First },
         { path: '2', component: Second },
         { path: '3', component: Third },
