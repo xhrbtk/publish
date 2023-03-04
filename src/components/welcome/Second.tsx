fe0cf766a0efe77bcdd53d508febed886310f0a2
xhtbtk <xhrbtk@gmail.com>
2023-03-04 21:24:40 +0800

    feat:四个欢迎页面ok

--- src/components/welcome/Second.tsx
+++ src/components/welcome/Second.tsx
@@ -1,13 +1,21 @@
 import { defineComponent, ref } from "vue";
-
+import s from './First.module.scss';
+import clock from '../../assets/icons/clock.svg'
+import { RouterLink } from 'vue-router';
 export const Second = defineComponent({
     setup() {
         return () => (
-        <>
-           <div >
-            Second
+           <div class={s.wrapper}>
+                <div class={s.card}>
+                    <img class={s.icon} src={clock} alt="" />
+                    <h2>每日提醒<br/>不遗漏每一笔账单</h2>
+                </div>
+                <div class={s.actions}>
+                    <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+                    <RouterLink to="/welcome/3" >下一页</RouterLink>
+                    <RouterLink to="/start" >跳过</RouterLink>
+                </div>
            </div>
-        </>
         )
     }
 })
