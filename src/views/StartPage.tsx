504ad98253f0d95d23a506d4144b1b3666dfa5a9
xhtbtk <xhrbtk@gmail.com>
2023-04-28 19:41:37 +0800

    feat:添加center组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -2,13 +2,19 @@ import { defineComponent } from "vue";
 import s from './StartPage.module.scss'
 import { Button } from "../shared/Button";
 import { FloatButton } from "../shared/FloatButton";
+import { Center } from "../shared/Center";
+import { Icon } from '../shared/Icon';
 export const StartPage = defineComponent({
     setup: (props, context) => {
         const onClick = () => {
             console.log('hi')
         }
         return () => (
-            <div>
+            <div class={s.start}>
+                <nav>menu</nav>
+                <Center class={s.pig_wrapper}>
+                    <Icon name="pig" class={s.pig} />
+                </Center>
                 <div class={s.button_wrapper}>
                     <Button class={s.button} onClick={onClick}>test</Button>
                 </div>
