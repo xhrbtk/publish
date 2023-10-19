7ebd62bf9c53627b75a82823f1c9cd908fd44dd6
xhtbtk <xhrbtk@gmail.com>
2023-10-19 20:38:28 +0800

    fix:startpage

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,41 +1,42 @@
-import { defineComponent, ref } from "vue";
-import s from './StartPage.module.scss'
-import { Button } from "../shared/Button";
-import { FloatButton } from "../shared/FloatButton";
-import { Navbar } from "../shared/Navbar";
-import { Center } from "../shared/Center";
-import { Icon } from '../shared/Icon';
-import { Overlay } from "../shared/Overlay";
+import { defineComponent, ref } from 'vue';
 import { RouterLink } from 'vue-router';
-import { MainLayout } from "../layouts/MainLayout";
+import { MainLayout } from '../layouts/MainLayout';
+import { Button } from '../shared/Button';
+import { Center } from '../shared/Center';
+import { FloatButton } from '../shared/FloatButton';
+import { Icon } from '../shared/Icon';
+import { Navbar } from '../shared/Navbar';
+import { Overlay } from '../shared/Overlay';
+import s from './StartPage.module.scss';
 export const StartPage = defineComponent({
-    setup: (props, context) => {
-        const refOverlayVisible = ref(false)
-        const onClickMenu = () => {
-            refOverlayVisible.value = !refOverlayVisible.value
-            console.log('refOverlayVisible', refOverlayVisible.value)
-        }
-        return () => (
-            <MainLayout>{
-                {
-                    title: () => '山竹记账',
-                    icon: <Icon name="menu" class={s.navIcon} onClick={onClickMenu}></Icon>,
-                    default: () => <>
-                        <Center class={s.pig_wrapper}>
-                        <Icon name="pig" class={s.pig} />
-                        </Center>
-                        <div class={s.button_wrapper}>
-                            <RouterLink to="/items/create">
-                                <Button class={s.button}>开始记账</Button>
-                            </RouterLink>
-                        </div>
-                        <RouterLink to="/items/create">
-                            <FloatButton iconName='add'/>
-                        </RouterLink>
-                        { refOverlayVisible.value && <Overlay onClose={() => refOverlayVisible.value = false}></Overlay> }
-                    </>
-                }
-            }</MainLayout>
-        )
+  setup: (props, context) => {
+    const refOverlayVisible = ref(false)
+    const onClickMenu = () => {
+      refOverlayVisible.value = !refOverlayVisible.value
     }
+    return () => (
+      <MainLayout>{
+        {
+          title: () => '山竹记账',
+          icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />,
+          default: () => <>
+            <Center class={s.pig_wrapper}>
+              <Icon name="pig" class={s.pig} />
+            </Center>
+            <div class={s.button_wrapper}>
+              <RouterLink to="/items/create">
+                <Button class={s.button}>开始记账</Button>
+              </RouterLink>
+            </div>
+            <RouterLink to="/items/create">
+              <FloatButton iconName='add' />
+            </RouterLink>
+            {refOverlayVisible.value &&
+              <Overlay onClose={() => refOverlayVisible.value = false} />
+            }
+          </>
+        }
+      }</MainLayout>
+    )
+  }
 })
