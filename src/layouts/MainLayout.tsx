f9f56527d8476de422dbe9efc367965746ec44aa
xhtbtk <xhrbtk@gmail.com>
2023-10-25 21:17:36 +0800

    feat:完成记一笔的样式

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,18 +1,17 @@
 import { defineComponent, PropType, ssrContextKey } from "vue";
 import { Navbar } from '../shared/Navbar';
+import s from './MainLayout.module.scss';
 export const MainLayout = defineComponent({
     setup: (props, context) => {
         return () => (
             <div>
-            <Navbar>
-                {
+                <Navbar>{
                     {
-                        default: ()=> context.slots.title?.(),
+                        default: () => context.slots.title?.(),
                         icon: () => context.slots.icon?.(),
                     }
-                }
-            </Navbar>
-            {context.slots.default?.()}
+                }</Navbar>
+                {context.slots.default?.()}
             </div>
         )
     }
